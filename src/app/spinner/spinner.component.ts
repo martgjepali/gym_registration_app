a6e87de5ca88940bd2b1fcfbf4e6098824be42a3
Mart Gjepali <mgjepali@gruppocdm.it>
2023-05-04 16:13:28 +0200

    feat: spinner color update

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-spinner',
